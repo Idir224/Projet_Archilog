a7ffafa98a7ae6156498ed497c89b74040475490
Idir SMAIL <idir.smail224@gmail.com>
2025-05-27 15:22:50 +0200

    aaa

--- archilog-0.2/src/archilog/views/__init__.py
+++ archilog-0.2/src/archilog/views/__init__.py
@@ -1,9 +1,13 @@
 from flask import Flask
+from archilog.views.gui import web_ui
 from archilog.config import config
-from archilog.views import gui, cli  # Si tu ajoutes API plus tard, ajoute-le ici
 
 def create_app():
     app = Flask(__name__)
-    app.config.from_prefixed_env(prefix="ARCHILOG_FLASK")
-    app.register_blueprint(gui.web_ui, url_prefix="/")
+
+    app.config["SQLALCHEMY_DATABASE_URI"] = config.DATABASE_URL
+    app.config["DEBUG"] = config.DEBUG
+
+    app.register_blueprint(web_ui, url_prefix="/")
+
     return app
